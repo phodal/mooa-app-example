654b658e6cb8cfacd4fe09bb72820ed949253bd0
Phodal HUANG <h@phodal.com>
2018-03-14 09:17:53 +0800

    [T] create apps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import {RouterModule, Routes} from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
+const appRoutes: Routes = [
+  { path: '*', component: AppComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'welcome', component: WelcomeComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
